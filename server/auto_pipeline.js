c30e579f1ffdbe392f7ed4496f9be092c23f58b7
NgocVuu <ngocvu14.3.2001@gmail.com>
2026-05-26 08:58:41 +0700

    fix: robust CORS handling for Fake JDownloader to support OPTIONS preflight and headers

--- server/auto_pipeline.js
+++ server/auto_pipeline.js
@@ -66,13 +66,16 @@ async function processAbyssQueue() {
 
 // -- 1. SETUP FAKE JDOWNLOADER SERVER --
 const app = express();
-app.use(cors());
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
 
